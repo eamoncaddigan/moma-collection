69f432ae99df088c3ba32a716044218557da7b8f
eamoncaddigan <eamon.caddigan@gmail.com>
2015-07-29 22:09:16 -0400

    Spent all that effort dealing with language stuff, but it wasn't really necessary.

--- countries/scrapeCountryInfo.R
+++ countries/scrapeCountryInfo.R
@@ -1,12 +1,11 @@
-# Here's a BUNCH of code to pull in the tables I want for mapping countries to
-# adjectivals and languages and ISO-639-1 codes. Probably deserves its own
-# package.
+# Here's some code to pull in the tables I want for mapping countries to 
+# adjectivals and their ISO 3166-1 alpha-2 codes
 
 # For now, the CSVs this writes still need a little manual touch-up. It's not
 # much data tho.
 
-library(rvest)
-library(dplyr)
+require(rvest)
+require(dplyr)
 
 getTableFromWeb <- function(url, xpath) {
   tableList <- url %>%
@@ -39,34 +38,43 @@ splitAdjectivals <- strsplit(countriesToAdjectivals[["adjectivals"]], ",[[:space
 for (i in seq_len(max(vapply(splitAdjectivals, length, 1)))) {
   countriesToAdjectivals[[paste("adjectival", i, sep="_")]] <- vapply(splitAdjectivals, function(x) { x[i] }, "")
 }
-write.csv(countriesToAdjectivals, "countries_to_adjectivals.csv", row.names = FALSE)
 
 
-# Countries to languages --------------------------------------------------
+# Countries and ISO 3166-1 alpha-2 codes ----------------------------------
 
-countriesToLanguages <- getTableFromWeb("http://www.infoplease.com/ipa/A0855611.html",
-                                        "//*[@id=\"Pg\"]/table[1]")
+countriesToCodes <- getTableFromWeb("https://en.wikipedia.org/wiki/ISO_3166-2",
+                                    "//*[@id=\"mw-content-text\"]/table[1]")
 
-colnames(countriesToLanguages) <- c("country_name", "languages")
-countriesToLanguages <- countriesToLanguages %>%
-  mutate(first_language = sub("[[:space:]]*[[:punct:][:digit:]].*", "", languages),
-         first_language = sub(" and .*", "", first_language))
-write.csv(countriesToLanguages, "countries_to_languages.csv", row.names = FALSE)
+colnames(countriesToCodes) <- c("iso3166", "country_name", "subdivisions")
+countriesToCodes <- countriesToCodes %>%
+  # Get rid of the wikipedia cruft
+  mutate_each(funs(gsub("\\[.*\\]", "", .))) %>% 
+  # Rearrange the country name into its natural order
+  mutate(natural_country_name = sub("([[:alpha:]]*), ([[:alpha:]].*)", 
+                                    "\\2 \\1", 
+                                    country_name))
 
 
-# Languages to ISO-639-1 codes --------------------------------------------
+# Do a full join on the tables so I can tidy up by hand -------------------
 
-languagesToCodes <- getTableFromWeb("https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes",
-                                    "//*[@id=\"mw-content-text\"]/table[2]")
-
-# Column names are difficult. Just hacking away here.
-languagesToCodes <- languagesToCodes[, c(3, 5)]
-colnames(languagesToCodes) <- c("language_name", "iso639")
-
-# Some languages have multiple names
-splitLanguageNames <- strsplit(languagesToCodes[["language_name"]], ",[[:space:]]*")
-for (i in seq_len(max(vapply(splitLanguageNames, length, 1)))) {
-  languagesToCodes[[paste("language_name_", i, sep="_")]] <- vapply(splitLanguageNames, function(x) { x[i] }, "")
+countriesCodesAdjectivals <- countriesToCodes %>%
+  select(natural_country_name, iso3166) %>%
+  full_join(countriesToAdjectivals, by="natural_country_name")
+if (file.exists("countries_codes_adjectivals.csv")) {
+  #write.csv(countriesCodesAdjectivals, "countries_codes_adjectivals.csv", row.names = FALSE)
 }
-write.csv(languagesToCodes, "languages_to_codes.csv", row.names = FALSE)
 
+
+# Map nationalities to codes ----------------------------------------------
+
+countriesCodesAdjectivals <- read.csv("countries_codes_adjectivals.csv")
+
+nationalitiesToCodes <- countriesCodesAdjectivals %>% 
+  select(-adjectivals) %>% 
+  gather("adjectival_number", "adjectival", adjectival_1:adjectival_4) %>% 
+  filter(!is.na(adjectival)) %>% 
+  select(natural_country_name, adjectival, iso3166) %>% 
+  gather("type", "nationality", natural_country_name, adjectival) %>% 
+  select(nationality, iso3166) %>% 
+  distinct()
+write.csv(nationalitiesToCodes, "nationalities_codes.csv", row.names = FALSE)
